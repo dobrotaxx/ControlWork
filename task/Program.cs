2ac9281019278c49b4b8d9938c8b0b5da4152545
Semyon <tonkih.sem@mail.ru>
2022-11-14 19:52:33 +0500

    added first method

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GenerateUserArray(string message)
+{
+    Console.Write(message);
+    int userArrayLength = Convert.ToInt32(Console.ReadLine());
+    string[] userArray = new string[userArrayLength];
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        Console.Write($"Введите значение для элемента массива с индексом {i}: ");
+        userArray[i] = Console.ReadLine().ToString();
+    }
+    return userArray;
+}
+
